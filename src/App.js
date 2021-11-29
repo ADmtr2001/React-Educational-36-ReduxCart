414fe0bd79d5b13d67a90d667f91b2c21e022d0b
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-29 18:58:24 +0200

    add store, actions, reducer

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import { createStore } from "redux";
+import { DECREASE, INCREASE } from "./actions";
+import reducer from "./reducer";
+
+import { Provider } from "react-redux";
+
+const initialStore = {
+  cart: cartItems,
+  total: 105,
+  amount: 5,
+};
+
+const store = createStore(reducer, initialStore);
 
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+      <CartContainer />
+    </Provider>
   );
 }
 
