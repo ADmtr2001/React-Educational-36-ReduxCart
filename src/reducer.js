7939f7f80b43b74d4ebb22bc9fe9f9dc3d9bf4ae
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-29 19:59:05 +0200

    add increase, decrease, clear list, remove item logic

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,42 @@
-import { DECREASE, INCREASE } from "./actions";
+import { CLEAR_CART, DECREASE, INCREASE, REMOVE } from "./actions";
+import cartItems from "./cart-items";
 
 function reducer(state, action) {
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
+  if (action.type === DECREASE) {
+    let tempCart = [];
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter(
+        (cartItem) => cartItem.id !== action.payload.id
+      );
+    } else {
+      tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+    }
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === INCREASE) {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === REMOVE) {
+    return {
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== action.payload.id),
+    };
+  }
+
   return state;
 }
 
